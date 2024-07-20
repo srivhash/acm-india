efeda483b6c5efc2b881f2f4187e09400fa91b69
Ashutosh <ashutosh.srivastava@students.iiit.ac.in>
2024-07-20 19:22:55 +0530

    description pages

---
+++ frontend/pages/about.js
@@ -0,0 +1,10 @@
+// pages/about.js
+import Head from 'next/head';
+
+export default function About() {
+  return (
+    <div>
+      About ACM India
+    </div>
+  );
+}
---
+++ frontend/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Random Homepage!</h1>
+            <p>This is just a placeholder for a random homepage.</p>
+            <p>Feel free to customize it with your own content!</p>
+        </div>
+    );
+};
+
+export default HomePage;
